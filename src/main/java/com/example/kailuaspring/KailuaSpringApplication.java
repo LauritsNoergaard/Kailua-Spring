4944c2f1bab28d296f0af0395f1990818ab3f09d
LauritsNoergaard <laur8709@stud.kea.dk>
2023-04-19 09:03:50 +0200

    Initial commit

---
+++ src/main/java/com/example/kailuaspring/KailuaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kailuaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KailuaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KailuaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kailuaspring/KailuaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kailuaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KailuaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
