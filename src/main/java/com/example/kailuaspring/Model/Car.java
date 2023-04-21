a66d8cad2d6d26766101bca6405c436d13ee9d4e
LauritsNoergaard <laur8709@stud.kea.dk>
2023-04-21 10:17:10 +0200

    Los clasos - Madsen

---
+++ src/main/java/com/example/kailuaspring/Model/Car.java
@@ -0,0 +1,11 @@
+package com.example.kailuaspring.Model;
+
+public class Car {
+    private String registrationNumber;
+    private String brand;
+    private String model;
+    private String fuelType;
+    private int odometer;
+    private String registrationDate;
+    private Type type;
+}
