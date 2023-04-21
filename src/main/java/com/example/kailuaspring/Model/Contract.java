01b70b1e2f62a17b4c1da1ac8797c3011a051661
Nademtis <Mathias.99.madsen@gmail.com>
2023-04-21 10:24:27 +0200

    Constructors / getter-setter added in Car.java, Contract.java and Renter.java - MADSEN

--- src/main/java/com/example/kailuaspring/Model/Contract.java
+++ src/main/java/com/example/kailuaspring/Model/Contract.java
@@ -7,4 +7,63 @@ public class Contract {
     private String driverLicenseNumber;
     private String registrationNumber;
     private int odometerAtStart;
+
+    public Contract(String contractID, String fromDateTime, String toDateTime, String driverLicenseNumber, String registrationNumber, int odometerAtStart) {
+        this.contractID = contractID;
+        this.fromDateTime = fromDateTime;
+        this.toDateTime = toDateTime;
+        this.driverLicenseNumber = driverLicenseNumber;
+        this.registrationNumber = registrationNumber;
+        this.odometerAtStart = odometerAtStart;
+    }
+
+    public String getContractID() {
+        return contractID;
+    }
+
+    public void setContractID(String contractID) {
+        this.contractID = contractID;
+    }
+
+    public String getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(String fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public String getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(String toDateTime) {
+        this.toDateTime = toDateTime;
+    }
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getOdometerAtStart() {
+        return odometerAtStart;
+    }
+
+    public void setOdometerAtStart(int odometerAtStart) {
+        this.odometerAtStart = odometerAtStart;
+    }
+
+
 }
--- src/main/java/com/example/kailuaspring/Model/Renter.java
+++ src/main/java/com/example/kailuaspring/Model/Renter.java
@@ -9,4 +9,79 @@ public class Renter {
     private String mobilePhone;
     private String phone;
     private String email;
+
+    public Renter(String driverLicenseNumber, String name, String address, String zip, String city, String mobilePhone, String phone, String email) {
+        this.driverLicenseNumber = driverLicenseNumber;
+        this.name = name;
+        this.address = address;
+        this.zip = zip;
+        this.city = city;
+        this.mobilePhone = mobilePhone;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
