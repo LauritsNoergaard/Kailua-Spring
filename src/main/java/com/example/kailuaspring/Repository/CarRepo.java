e6132dad3231a8cf58e0cbb0442f0df7886a07a5
LauritsNoergaard <laur8709@stud.kea.dk>
2023-04-21 14:03:08 +0200

    Rent car SQL

--- src/main/java/com/example/kailuaspring/Repository/CarRepo.java
+++ src/main/java/com/example/kailuaspring/Repository/CarRepo.java
@@ -1,4 +1,25 @@
 package com.example.kailuaspring.Repository;
 
+import com.example.kailuaspring.Model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CarRepo {
+
+    @Autowired
+    JdbcTemplate template;
+    public List<Car> fetchAvailableCars (String from_date, String to_date, String carType){ //Should type be ENUM??
+        String sql = "SELECT * FROM car" +
+                "INNER JOIN contract ON car.registration_number = contract.registration_number" +
+                "WHERE from_date_time > '?' OR to_date_time < '?' AND rental_type = '?';";
+        RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
+        List<Car> lists = template.query(sql, rowMapper, to_date, from_date, carType);
+        return lists;
+    }
 }
--- src/main/java/com/example/kailuaspring/Service/CarService.java
+++ src/main/java/com/example/kailuaspring/Service/CarService.java
@@ -1,4 +1,18 @@
 package com.example.kailuaspring.Service;
 
+import com.example.kailuaspring.Model.Car;
+import com.example.kailuaspring.Repository.CarRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+    @Autowired
+    CarRepo carRepo;
+
+    public List<Car> fetchAvailableCars(String fromDate, String toDate, String carType) {
+    return carRepo.fetchAvailableCars(fromDate, toDate, carType);
+    }
 }
